04afa8989329f1bb12b9735ed2ea811df9f0a6f8
heakknight <heakmeng42@gmail.com>
2026-04-01 15:27:00 +0700

    Added permission on UserController

--- src/main/java/com/phengmengheak/demojwtsecurity/controller/UserController.java
+++ src/main/java/com/phengmengheak/demojwtsecurity/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.phengmengheak.demojwtsecurity.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/users")
+@SecurityRequirement(name="bearerAuth")
+@PreAuthorize("hasRole('ADMIN')")
 public class UserController {
     @GetMapping
     public String getAllUsers(){
